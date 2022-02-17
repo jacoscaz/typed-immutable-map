288c0fcec6931553b04827e27171e87ed4816463
Jacopo Scazzosi <jacopo@scazzosi.com>
2022-02-17 13:08:46 +0100

    simplifies from* methods, fixes wrong default value in get()

--- src/HashMap/get.ts
+++ src/HashMap/get.ts
@@ -4,7 +4,7 @@ import type { KeyEquals } from './primitives';
 import { HashMap } from './HashMap';
 import { getHash, findHash } from './primitives';
 
-export function get<K, V>(key: K, map: HashMap<K, V>, keyEquals?: KeyEquals<K>): V | null {
+export function get<K, V>(key: K, map: HashMap<K, V>, keyEquals?: KeyEquals<K>): V | undefined {
   const hash = findHash(key);
-  return getHash(null, hash, key, map, keyEquals);
+  return getHash(undefined, hash, key, map, keyEquals);
 }
